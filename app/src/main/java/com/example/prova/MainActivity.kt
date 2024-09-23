4b2d0a21a0c0bfc42ea00ce5dca68bb73135dbe0
teste <teste@gmail.com>
2024-09-23 08:56:43 -0300

    teste

---
+++ app/src/main/java/com/example/prova/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.prova
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.prova.ui.theme.ProvaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+
+}
